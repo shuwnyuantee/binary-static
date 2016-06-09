4320ae45147fdd147b3f280568ad97cf7cf2c834
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-06-09 13:51:24 +0800

    translation

--- scripts/translation.sh
+++ scripts/translation.sh
@@ -18,8 +18,8 @@ msgmerge --previous --backup none --no-wrap --update ../src/config/en.po ../src/
 carton exec perl -pi -e 's/Content-Type: text\/plain; charset=CHARSET/Content-Type: text\/plain; charset=UTF-8/'  ../src/config/locales/messages.pot
 
 
-carton exec perl -ni -e  'print unless m/^#:.+:\d+$/'  ../src/config/en.po
+carton exec perl -ni -e  'print unless m/(^#:|^#\.)/'  ../src/config/en.po
 
 for i in $(ls ../src/config/locales/*.po*); do \
-    carton exec perl -ni -e  'print unless m/^#:.+:\d+$/'  $i ; \
+    carton exec perl -ni -e  'print unless m/(^#:|^#\.)/'  $i ; \
 done
